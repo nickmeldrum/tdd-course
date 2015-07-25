1c8f7c25c5b2df890f8dcdcda8785826f183b662
Nick Meldrum <nick@nickmeldrum.com>
2015-07-25 11:12:39 +0100

    created account class

--- test/example1-bank-transfer-spec.js
+++ test/example1-bank-transfer-spec.js
@@ -2,21 +2,30 @@ var should = require('chai').should(),
     bank = require('../lib/example1-bank-transfer');
 
 describe('#bank', function() {
+    describe('#account', function() {
+        it('create account returns account with correct balance', function() {
+            var account = bank.createAccount(12);
+            account.balance.should.equal(12);
+        });
+
+        it('credit account with 5 things has balance of 5 more thingys', function() {
+            var account = bank.createAccount(12);
+            account.credit(5);
+            account.balance.should.equal(17);
+        });
+    });
+    
     describe('transfer 5 thingys', function() {
         beforeEach(function() {
-            this.payerBalance = 10;
-            this.payeeBalance = 10;
-            this.transferAmount = 5;
+            this.result = bank.transfer(10, 10, 5);
         });
 
         it('reduces the balance of the payer', function() {
-            this.payerBalance = bank.changeBalance(this.payerBalance, -this.transferAmount);
-            this.payerBalance.should.equal(5);
+            this.result.payerBalance.should.equal(5);
         });
 
         it('increases the balance of the payee', function() {
-            this.payeeBalance = bank.changeBalance(this.payeeBalance, this.transferAmount);
-            this.payeeBalance.should.equal(15);
+            this.result.payeeBalance.should.equal(15);
         });
     });
 });
