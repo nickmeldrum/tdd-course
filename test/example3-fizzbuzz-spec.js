fab8f87fb3c92aaa2d6a905a50440a5fc8bbb436
Nick Meldrum <nick@nickmeldrum.com>
2015-07-25 13:11:49 +0100

    refactored second test

--- test/example3-fizzbuzz-spec.js
+++ test/example3-fizzbuzz-spec.js
@@ -2,14 +2,18 @@ var should = require('chai').should(),
     fizzbuzz = require('../lib/example3-fizzbuzz');
 
 describe('#fizzbuzz', function() {
+    var result;
+
+    beforeEach(function() {
+        result = fizzbuzz().split(',');
+    });
+
     it('first number is a 1', function() {
-        var result = fizzbuzz();
         result[0].should.equal('1');
     });
 
     it('second number is a 2', function() {
-        var result = fizzbuzz();
-        result.split(',')[1].should.equal('2');
+        result[1].should.equal('2');
     });
 });
 
